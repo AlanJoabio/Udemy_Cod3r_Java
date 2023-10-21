17de965e0e605ef2d1430135f23eeea7c61ac708
LanJob <alanjmsq@gmail.com>
2023-10-21 11:30:40 -0300

    Aula If/Else

--- exercicios/src/controle/DesafioIf.java
+++ exercicios/src/controle/DesafioIf.java
@@ -5,6 +5,7 @@ public class DesafioIf {
 		
 		double nota = 1.3;
 		
+		//não usar ";" em estrutura de controle (tem uma exceção)
 		if(nota >= 9.0){
 			System.out.println("Quadro de Honra!!");
 			System.out.println("Você é fera!!!");
