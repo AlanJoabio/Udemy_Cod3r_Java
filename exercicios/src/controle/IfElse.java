83a8b7da6f0fe153d93d34448b398c66eece6c0c
LanJob <alanjmsq@gmail.com>
2023-10-22 19:55:49 -0300

    Aula IfElse

--- exercicios/src/controle/IfElse.java
+++ exercicios/src/controle/IfElse.java
@@ -8,20 +8,19 @@ public class IfElse {
 		String valor = JOptionPane.showInputDialog(
 				"Informe o número:");
 		int numero = Integer.parseInt(valor);
-		
+//		
+//		if(numero % 2 == 0) {
+//			System.out.println("Número par!");
+//		}
+//		
+//		if(numero % 2 == 1) {
+//			System.out.println("Número ímpar!");
+//		}
+//		
 		if(numero % 2 == 0) {
 			System.out.println("Número par!");
-		}
-		
-		if(numero % 2 == 1) {
-			System.out.println("Número ímpar!");
-		}
-		
-		if(numero % 2 == 0) {
-			System.out.println("Número par!");
-		} else {
-			
-			
+		} else {			
+			System.out.println("Número impar!");
 		}
 				
 	}
